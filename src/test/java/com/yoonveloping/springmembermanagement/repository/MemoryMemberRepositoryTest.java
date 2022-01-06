5080e90d8a996e84b561c89728c4898aa00d76d9
Yunkeun <yunx2@naver.com>
2022-01-06 18:04:19 +0900

    feat: 기능별 테스트 코드 추가

---
+++ src/test/java/com/yoonveloping/springmembermanagement/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.yoonveloping.springmembermanagement.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.yoonveloping.springmembermanagement.domain.Member;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+	MemoryMemberRepository repository = new MemoryMemberRepository();
+
+	@AfterEach
+	public void afterEach() {
+		repository.clearStore();
+	}
+
+	@Test
+	void save() {
+		// given
+		Member member1 = new Member();
+		member1.setName("Yunkeun");
+		// when
+		repository.save(member1);
+		Member result = repository.findById(member1.getId()).get();
+		// then
+		assertThat(member1).isEqualTo(result);
+	}
+
+	@Test
+	void findByName() {
+		// given
+		Member member1 = new Member();
+		member1.setName("Cho1");
+
+		Member member2 = new Member();
+		member2.setName("Cho2");
+		// when
+		repository.save(member1);
+		repository.save(member2);
+		Member result = repository.findByName("Cho1").get();
+		// then
+		assertThat(member1).isEqualTo(result);
+	}
+
+	@Test
+	void findAll() {
+		// given
+		Member member1 = new Member();
+		member1.setName("Cho1");
+
+		Member member2 = new Member();
+		member2.setName("Cho2");
+		// when
+		repository.save(member1);
+		repository.save(member2);
+		List<Member> result = repository.findAll();
+		// then
+		assertThat(result.size()).isEqualTo(2);
+	}
+}
