4f1b4d358bfe11e5f30aaa616458ca7bd2b53955
Yunkeun <yunx2@naver.com>
2022-01-06 16:38:51 +0900

    Initial commit

---
+++ src/test/java/com/yoonveloping/springmembermanagement/SpringMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonveloping.springmembermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
