97397b0a847194c9466f167791622f0dfb70b11b
Yunkeun <yunx2@naver.com>
2022-01-09 14:17:42 +0900

    feat: 모든 메소드의 호출 시간 측정

--- src/main/java/com/yoonveloping/springmembermanagement/service/MemberService.java
+++ src/main/java/com/yoonveloping/springmembermanagement/service/MemberService.java
@@ -20,9 +20,17 @@ public class MemberService {
 	 * 회원 등록
 	 */
 	public Long join(Member member) {
-		validateDuplicateMember(member); // 중복 회원 검증
-		memberRepository.save(member);
-		return member.getId();
+		long start = System.currentTimeMillis();
+		try {
+			validateDuplicateMember(member); // 중복 회원 검증
+			memberRepository.save(member);
+			return member.getId();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("join " + timeMs + "ms");
+		}
+
 	}
 
 	private void validateDuplicateMember(Member member) {
@@ -36,13 +44,27 @@ public class MemberService {
 	 * 전체 회원 조회
 	 */
 	public List<Member> findMembers() {
-		return memberRepository.findAll();
+		long start = System.currentTimeMillis();
+		try {
+			return memberRepository.findAll();
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("findMembers " + timeMs + "ms");
+		}
 	}
 
 	/**
 	 * id로 회원 조회
 	 */
 	public Optional<Member> findOne(Long memberId) {
-		return memberRepository.findById(memberId);
+		long start = System.currentTimeMillis();
+		try {
+			return memberRepository.findById(memberId);
+		} finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+			System.out.println("findMember by id " + timeMs + "ms");
+		}
 	}
 }
