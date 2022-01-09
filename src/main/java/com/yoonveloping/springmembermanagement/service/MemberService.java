29ed5a10c13a077e000899eea9a7c294574b60ff
Yunkeun <yunx2@naver.com>
2022-01-09 14:51:00 +0900

    refactor: Aop 이용하여 시간 측정

--- src/main/java/com/yoonveloping/springmembermanagement/service/MemberService.java
+++ src/main/java/com/yoonveloping/springmembermanagement/service/MemberService.java
@@ -15,22 +15,13 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-
 	/**
 	 * 회원 등록
 	 */
 	public Long join(Member member) {
-		long start = System.currentTimeMillis();
-		try {
-			validateDuplicateMember(member); // 중복 회원 검증
-			memberRepository.save(member);
-			return member.getId();
-		} finally {
-			long finish = System.currentTimeMillis();
-			long timeMs = finish - start;
-			System.out.println("join " + timeMs + "ms");
-		}
-
+		validateDuplicateMember(member); // 중복 회원 검증
+		memberRepository.save(member);
+		return member.getId();
 	}
 
 	private void validateDuplicateMember(Member member) {
@@ -44,27 +35,13 @@ public class MemberService {
 	 * 전체 회원 조회
 	 */
 	public List<Member> findMembers() {
-		long start = System.currentTimeMillis();
-		try {
-			return memberRepository.findAll();
-		} finally {
-			long finish = System.currentTimeMillis();
-			long timeMs = finish - start;
-			System.out.println("findMembers " + timeMs + "ms");
-		}
+		return memberRepository.findAll();
 	}
 
 	/**
 	 * id로 회원 조회
 	 */
 	public Optional<Member> findOne(Long memberId) {
-		long start = System.currentTimeMillis();
-		try {
-			return memberRepository.findById(memberId);
-		} finally {
-			long finish = System.currentTimeMillis();
-			long timeMs = finish - start;
-			System.out.println("findMember by id " + timeMs + "ms");
-		}
+		return memberRepository.findById(memberId);
 	}
 }
